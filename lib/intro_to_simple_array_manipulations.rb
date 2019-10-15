29385433c35ad66091ca126fce212a32d09394cf
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-15 20:42:22 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop
+  array.pop()
+end
+
+def pop_with_arg(array)
+  array.pop(2)
+end
+
+
+
