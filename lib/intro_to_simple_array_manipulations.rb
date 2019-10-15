942f302bebe2455725c516d55df4e838d70e4159
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-15 21:11:24 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,75 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
+end 
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(ar1, ar2)
+  ar1.concat(ar2)
+end
+
+def using_insert(ar, el)
+  ar.insert(4, el)
+end
+
+def using_uniq(ar)
+  ar.uniq
+end
+
+def using_flatten(ar)
+  ar.flatten
+end
+
+def using_delete(ar, string)
+  ar.delete(string)
+end
+
+def using_delete_at(ar, index)
+  ar.delete_at(index)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
